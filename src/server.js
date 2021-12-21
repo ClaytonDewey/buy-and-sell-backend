db6595dadc940096dcb48b76766858920e6c2f2b
Clay <clay@dryadmedia.com>
2021-12-21 13:30:12 -0600

    Rewrite the listings endpoints

--- src/server.js
+++ src/server.js
@@ -5,29 +5,29 @@ import { db } from './database';
 let server;
 
 const start = async () => {
-  server = Hapi.server({
-    port: 8000,
-    host: 'localhost',
-  });
+    server = Hapi.server({
+        port: 8000,
+        host: 'localhost',
+    });
 
-  routes.forEach(route => server.route(route));
+    routes.forEach(route => server.route(route));
 
-  db.connect();
-  await server.start();
-  console.log(`Server is listening on ${server.info.uri}`);
+    db.connect();
+    await server.start();
+    console.log(`Server is listening on ${server.info.uri}`);
 }
 
 process.on('unhandledRejection', err => {
-  console.log(err);
-  process.exit(1);
+    console.log(err);
+    process.exit(1);
 });
 
 process.on('SIGINT', async () => {
-  console.log('Stopping server...');
-  await server.stop({ timeout: 10000 });
-  db.end();
-  console.log('Server stopped');
-  process.exit(0);
+    console.log('Stopping server...');
+    await server.stop({ timeout: 10000 });
+    db.end();
+    console.log('Server stopped');
+    process.exit(0);
 });
 
 start();
